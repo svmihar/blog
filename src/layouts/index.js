0e5b639300e0e9a309db0900db286440d909c8c1
svmihar <svmihar@gmail.com>
2018-08-13 15:54:29 +0700

    updateing favicon.png

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import favicon from '../../static/img/favicon.png'
 
 import Navbar from '../components/Navbar'
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="svmihar 🙃" />
+    <Helmet 
+      title="- svmihar's scribbles" 
+      meta={[
+        { name: 'description', content: 'blog' },
+        { name: 'keywords', content: 'webdev, videography, gatsbyjs, reactjs, frontend' },
+        ]}
+      link={[
+        { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+    ]}/>
     <Navbar />
     <div>{children()}</div>
   </div>
