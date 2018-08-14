be54eeb86f7e4b66e1be597e9250fa3d4eb5c86a
svmihar <svmihar@gmail.com>
2018-08-14 12:24:37 +0700

    adding 404 image on not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
+
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>Lahhhh ndak nemu.</p>
+    <img class="center" src="https://image.ibb.co/jijngU/404.jpg"></img>
   </div>
 )
 
